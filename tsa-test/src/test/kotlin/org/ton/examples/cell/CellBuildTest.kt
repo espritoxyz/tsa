0898f0c72857a0a7537362d92efedfd80a5246b3
metametamoon <sorvinlevs@gmail.com>
2025-09-01 14:00:25 +0300

    Added a test for symbolic parameter of stones

--- tsa-test/src/test/kotlin/org/ton/examples/cell/CellBuildTest.kt
+++ tsa-test/src/test/kotlin/org/ton/examples/cell/CellBuildTest.kt
@@ -1,13 +1,35 @@
 package org.ton.examples.cell
 
+import org.ton.test.gen.dsl.render.TsRenderer
+import org.ton.test.utils.TvmTestExecutor
 import org.ton.test.utils.compareSymbolicAndConcreteFromResource
+import org.ton.test.utils.extractResource
+import org.ton.test.utils.funcCompileAndAnalyzeAllMethods
+import org.ton.test.utils.propertiesFound
+import org.usvm.test.resolver.TvmMethodFailure
 import kotlin.test.Test
 
 class CellBuildTest {
     private val storeSliceConstFif: String = "/cell/cell-build/CellBuild.fif"
+    private val storeOnesFunc: String = "/cell/cell-build/StonesSymbolicSize.fc"
 
     @Test
     fun cellBuildTest() {
         compareSymbolicAndConcreteFromResource(testPath = storeSliceConstFif, lastMethodIndex = 9)
+    }
+
+    @Test
+    fun cellStonesSymbolicTest() {
+        val path = extractResource(storeOnesFunc)
+        val result = funcCompileAndAnalyzeAllMethods(path)
+
+        propertiesFound(
+            result.testSuites.single(),
+            listOf(
+                { test -> (test.result as? TvmMethodFailure)?.exitCode == 129 },
+                { test -> (test.result as? TvmMethodFailure)?.exitCode == 130 },
+            )
+        )
+        TvmTestExecutor.executeGeneratedTests(result, path, TsRenderer.ContractType.Func)
     }
 }
