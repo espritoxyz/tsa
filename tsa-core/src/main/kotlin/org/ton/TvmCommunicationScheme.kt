a46c34e5f2e17a3df6db977dce02ba0c22103a06
tochilinak <tochilina.2002@yandex.ru>
2025-09-15 13:31:41 +0300

    .

--- tsa-core/src/main/kotlin/org/ton/TvmCommunicationScheme.kt
+++ tsa-core/src/main/kotlin/org/ton/TvmCommunicationScheme.kt
@@ -1,5 +1,6 @@
 package org.ton
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import org.usvm.machine.state.ContractId
@@ -13,6 +14,22 @@ data class TvmContractHandlers(
 )
 
 @Serializable
+sealed interface DestinationDescription
+
+@Serializable
+@SerialName("linear")
+data class LinearDestinations(
+    val destinations: List<ContractId>,
+) : DestinationDescription
+
+@Serializable
+@SerialName("out_opcodes")
+data class OpcodeToDestination(
+    val outOpcodeToDestination: Map<String, ContractId>,
+    val other: ContractId? = null,
+) : DestinationDescription
+
+@Serializable
 data class TvmHandlerDestinations(
     val op: String,
     val destinations: List<ContractId>,
@@ -20,3 +37,7 @@ data class TvmHandlerDestinations(
 
 fun communicationSchemeFromJson(json: String): Map<ContractId, TvmContractHandlers> =
     Json.decodeFromString<List<TvmContractHandlers>>(json).associateBy { it.id }
+
+fun main() {
+
+}
