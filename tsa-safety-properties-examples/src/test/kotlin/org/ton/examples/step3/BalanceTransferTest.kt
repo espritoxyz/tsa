710f8df8396d42ef5957f6692bcfa66154cf4ae8
tochilinak <tochilina.2002@yandex.ru>
2025-09-17 12:59:59 +0300

    ignore testBalanceTransfer

--- tsa-safety-properties-examples/src/test/kotlin/org/ton/examples/step3/BalanceTransferTest.kt
+++ tsa-safety-properties-examples/src/test/kotlin/org/ton/examples/step3/BalanceTransferTest.kt
@@ -12,6 +12,7 @@ import org.usvm.test.resolver.TvmMethodFailure
 import org.usvm.test.resolver.TvmTestIntegerValue
 import org.usvm.test.resolver.TvmTestSliceValue
 import kotlin.io.path.readText
+import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -20,6 +21,7 @@ class BalanceTransferTest {
     private val checkerResourcePath = "/examples/step3/balance_transfer_checker.fc"
     private val intercontractSchemePath = "/examples/step3/wallet-intercontract-scheme.json"
 
+    @Ignore
     @Test
     fun testBalanceTransfer() {
         val walletContractPath = getResourcePath<BalanceTransferTest>(walletContractResourcePath)
