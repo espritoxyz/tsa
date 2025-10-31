d0ad922b9b24f4e57fdd1e89630dccf828d9a489
Alex <aleksandrgrefenstejn@gmail.com>
2025-10-31 12:33:59 +0300

    Linter

--- tsa-test/src/test/kotlin/org/ton/examples/fees/forward/forwardFeesTest.kt
+++ tsa-test/src/test/kotlin/org/ton/examples/fees/forward/forwardFeesTest.kt
@@ -1,33 +1,11 @@
 package org.ton.examples.checkers
 
-import org.ton.TvmContractHandlers
-import org.ton.bitstring.BitString
-import org.ton.bytecode.TsaContractCode
-import org.ton.cell.Cell
-import org.ton.communicationSchemeFromJson
-import org.ton.test.utils.FIFT_STDLIB_RESOURCE
 import org.ton.test.utils.checkInvariants
-import org.ton.test.utils.extractResource
 import org.ton.test.utils.propertiesFound
-import org.usvm.machine.IntercontractOptions
-import org.usvm.machine.TactSourcesDescription
-import org.usvm.machine.TvmConcreteContractData
 import org.usvm.machine.TvmContext
-import org.usvm.machine.TvmOptions
 import org.usvm.machine.analyzeInterContract
-import org.usvm.machine.getFuncContract
-import org.usvm.machine.getTactContract
-import org.usvm.machine.state.ContractId
-import org.usvm.test.resolver.TvmExecutionWithSoftFailure
 import org.usvm.test.resolver.TvmMethodFailure
-import org.usvm.test.resolver.TvmMethodSymbolicResult
-import org.usvm.test.resolver.TvmSuccessfulExecution
-import org.usvm.test.resolver.TvmSymbolicTest
-import org.usvm.test.resolver.TvmTestInput
-import kotlin.io.path.readText
 import kotlin.test.Test
-import kotlin.test.assertTrue
-import kotlin.time.Duration.Companion.seconds
 
 class CheckersTest {
     private val checker = "/fees/forward/checker.fc"
@@ -57,7 +35,7 @@ class CheckersTest {
             listOf { test ->
                 val checkerExitCode = test.eventsList.single { it.id == 0 }.methodResult
                 val checkerExitCodeGood = (checkerExitCode as? TvmMethodFailure)?.exitCode == 10000
-                checkerExitCodeGood 
+                checkerExitCodeGood
             },
         )
 
