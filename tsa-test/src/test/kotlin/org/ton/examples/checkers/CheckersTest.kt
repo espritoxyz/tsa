46fed217010c98f10f4ee67c65b74876129cf091
tochilinak <tochilina.2002@yandex.ru>
2025-10-31 15:19:57 +0300

    Refactored some tests + new test for

--- tsa-test/src/test/kotlin/org/ton/examples/checkers/CheckersTest.kt
+++ tsa-test/src/test/kotlin/org/ton/examples/checkers/CheckersTest.kt
@@ -2,11 +2,12 @@ package org.ton.examples.checkers
 
 import org.ton.TvmContractHandlers
 import org.ton.bitstring.BitString
-import org.ton.bytecode.TsaContractCode
 import org.ton.cell.Cell
 import org.ton.communicationSchemeFromJson
 import org.ton.test.utils.FIFT_STDLIB_RESOURCE
 import org.ton.test.utils.checkInvariants
+import org.ton.test.utils.extractCheckerContractFromResource
+import org.ton.test.utils.extractFuncContractFromResource
 import org.ton.test.utils.extractResource
 import org.ton.test.utils.propertiesFound
 import org.usvm.machine.IntercontractOptions
@@ -18,13 +19,13 @@ import org.usvm.machine.analyzeInterContract
 import org.usvm.machine.getFuncContract
 import org.usvm.machine.getTactContract
 import org.usvm.machine.state.ContractId
-import org.usvm.test.resolver.TvmExecutionWithSoftFailure
 import org.usvm.test.resolver.TvmMethodFailure
 import org.usvm.test.resolver.TvmMethodSymbolicResult
 import org.usvm.test.resolver.TvmSuccessfulExecution
 import org.usvm.test.resolver.TvmSymbolicTest
 import org.usvm.test.resolver.TvmTestInput
 import kotlin.io.path.readText
+import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertTrue
 import kotlin.time.Duration.Companion.seconds
@@ -35,18 +36,10 @@ class CheckersTest {
     private val externalCallCheckerWithCapture = "/checkers/send_external_with_capture.fc"
     private val balancePath = "/args/balance.fc"
     private val balanceExternalPath = "/args/balance_external.fc"
-    private val bounceCheckerPath = "/checkers/bounce.fc"
     private val getC4CheckerPath = "/checkers/get_c4.fc"
     private val emptyContractPath = "/empty_contract.fc"
-    private val senderBouncePath = "/args/send_bounce_true.fc"
-    private val recipientBouncePath = "/args/receive_bounce_msg.fc"
-    private val recipientBounceSoftFailurePath = "/args/receive_bounce_msg_with_soft_failure.fc"
-    private val bounceTestScheme = "/checkers/bounce-test-scheme.json"
     private val tactConfig = "/tact/tact.config.json"
     private val intBlastOptimizationChecker = "/checkers/int_optimization.fc"
-    private val bounceFormatContract = "/args/bounce_format_send.fc"
-    private val bounceFormatChecker = "/checkers/bounce_format.fc"
-    private val bounceFormatScheme = "/checkers/bounce_format_scheme.json"
 
     private object TransactionRollBackTestData {
         const val SENDER = "/checkers/transaction-rollback/sender-checker.fc"
@@ -54,23 +47,24 @@ class CheckersTest {
     }
 
     private object OnOutMessageTestData {
-        val checker = "/checkers/on-out-message-test/checker.fc"
-        val sender = "/checkers/on-out-message-test/sender.fc"
-        val receiver = "/checkers/on-out-message-test/receiver.fc"
-        val communicationScheme = "/checkers/on-out-message-test/communication-scheme.json"
+        const val CHECKER = "/checkers/on-out-message-test/checker.fc"
+        const val CHECKER_FAIL = "/checkers/on-out-message-test/checker_fail.fc"
+        const val SENDER = "/checkers/on-out-message-test/sender.fc"
+        const val RECEIVER = "/checkers/on-out-message-test/receiver.fc"
+        const val SCHEME = "/checkers/on-out-message-test/communication-scheme.json"
     }
 
     private object OnComputePhaseExitTestData {
-        val checker = "/checkers/on-compute-phase-exit-test/checker.fc"
-        val sender = "/checkers/on-compute-phase-exit-test/sender.fc"
-        val receiverNoThrow = "/checkers/on-compute-phase-exit-test/receiver-nothrow.fc"
-        val receiverThrow = "/checkers/on-compute-phase-exit-test/receiver-throw.fc"
-        val communicationScheme = "/checkers/on-compute-phase-exit-test/communication-scheme.json"
+        const val CHECKER = "/checkers/on-compute-phase-exit-test/checker.fc"
+        const val SENDER = "/checkers/on-compute-phase-exit-test/sender.fc"
+        const val RECEIVER_NO_THROW = "/checkers/on-compute-phase-exit-test/receiver-nothrow.fc"
+        const val RECEIVER_THROW = "/checkers/on-compute-phase-exit-test/receiver-throw.fc"
+        const val SCHEME = "/checkers/on-compute-phase-exit-test/communication-scheme.json"
     }
 
     private object OnComputePhaseExitStopOnFirstErrorTestData {
-        val checker = "/checkers/on-compute-phase-exit-stop-on-first-error-test/checker.fc"
-        val sender = "/checkers/on-compute-phase-exit-stop-on-first-error-test/sender.fc"
+        const val CHECKER = "/checkers/on-compute-phase-exit-stop-on-first-error-test/checker.fc"
+        const val SENDER = "/checkers/on-compute-phase-exit-stop-on-first-error-test/sender.fc"
     }
 
     @Test
@@ -212,10 +206,10 @@ class CheckersTest {
 
     @Test
     fun `on_out_message gets called`() {
-        val checkerContract = extractCheckerContractFromResource(OnOutMessageTestData.checker)
-        val senderContract = extractFuncContractFromResource(OnOutMessageTestData.sender)
-        val receiverContract = extractFuncContractFromResource(OnOutMessageTestData.receiver)
-        val communicationScheme = extractCommunicationSchemeFromResource(OnOutMessageTestData.communicationScheme)
+        val checkerContract = extractCheckerContractFromResource(OnOutMessageTestData.CHECKER)
+        val senderContract = extractFuncContractFromResource(OnOutMessageTestData.SENDER)
+        val receiverContract = extractFuncContractFromResource(OnOutMessageTestData.RECEIVER)
+        val communicationScheme = extractCommunicationSchemeFromResource(OnOutMessageTestData.SCHEME)
         val options = createIntercontractOptions(communicationScheme)
         val tests =
             analyzeInterContract(
@@ -235,20 +229,42 @@ class CheckersTest {
         )
     }
 
+    @Ignore("Behavior in on_out_message is invalid")
+    @Test
+    fun `fail in on_out_message is global`() {
+        val checkerContract = extractCheckerContractFromResource(OnOutMessageTestData.CHECKER_FAIL)
+        val senderContract = extractFuncContractFromResource(OnOutMessageTestData.SENDER)
+        val receiverContract = extractFuncContractFromResource(OnOutMessageTestData.RECEIVER)
+        val communicationScheme = extractCommunicationSchemeFromResource(OnOutMessageTestData.SCHEME)
+        val options = createIntercontractOptions(communicationScheme)
+        val tests =
+            analyzeInterContract(
+                listOf(checkerContract, senderContract, receiverContract),
+                startContractId = 0,
+                methodId = TvmContext.RECEIVE_INTERNAL_ID,
+                options = options,
+            )
+
+        propertiesFound(
+            tests,
+            listOf { test -> test.result.exitCode() == 300 },
+        )
+    }
+
     @Test
     fun `on_compute_phase_exit gets called without exception`() {
-        onComputePhaseBaseTest(OnComputePhaseExitTestData.receiverNoThrow)
+        onComputePhaseBaseTest(OnComputePhaseExitTestData.RECEIVER_NO_THROW)
     }
 
     @Test
     fun `on_compute_phase_exit gets called with exception`() {
-        onComputePhaseBaseTest(OnComputePhaseExitTestData.receiverThrow)
+        onComputePhaseBaseTest(OnComputePhaseExitTestData.RECEIVER_THROW)
     }
 
     @Test
     fun `on_compute_phase_exit isn't called with exception when stopFirstError == true`() {
-        val checkerContract = extractCheckerContractFromResource(OnComputePhaseExitStopOnFirstErrorTestData.checker)
-        val senderContract = extractFuncContractFromResource(OnComputePhaseExitStopOnFirstErrorTestData.sender)
+        val checkerContract = extractCheckerContractFromResource(OnComputePhaseExitStopOnFirstErrorTestData.CHECKER)
+        val senderContract = extractFuncContractFromResource(OnComputePhaseExitStopOnFirstErrorTestData.SENDER)
         val options = TvmOptions(stopOnFirstError = true)
         val tests =
             analyzeInterContract(
@@ -269,10 +285,10 @@ class CheckersTest {
     }
 
     private fun onComputePhaseBaseTest(receiverContractPath: String) {
-        val checkerContract = extractCheckerContractFromResource(OnComputePhaseExitTestData.checker)
-        val senderContract = extractFuncContractFromResource(OnComputePhaseExitTestData.sender)
+        val checkerContract = extractCheckerContractFromResource(OnComputePhaseExitTestData.CHECKER)
+        val senderContract = extractFuncContractFromResource(OnComputePhaseExitTestData.SENDER)
         val receiverContract = extractFuncContractFromResource(receiverContractPath)
-        val communicationScheme = extractCommunicationSchemeFromResource(OnComputePhaseExitTestData.communicationScheme)
+        val communicationScheme = extractCommunicationSchemeFromResource(OnComputePhaseExitTestData.SCHEME)
         val options = createIntercontractOptions(communicationScheme)
         val tests =
             analyzeInterContract(
@@ -294,135 +310,12 @@ class CheckersTest {
             else -> error("Soft failure in a test")
         }
 
-    private fun extractCheckerContractFromResource(checkerResourcePath: String): TsaContractCode {
-        val checkerPath = extractResource(checkerResourcePath)
-        val checkerContract = getFuncContract(checkerPath, FIFT_STDLIB_RESOURCE, isTSAChecker = true)
-        return checkerContract
-    }
-
-    private fun extractFuncContractFromResource(contractResourcePath: String): TsaContractCode {
-        val contractPath = extractResource(contractResourcePath)
-        val checkerContract = getFuncContract(contractPath, FIFT_STDLIB_RESOURCE)
-        return checkerContract
-    }
-
     private fun extractCommunicationSchemeFromResource(
         communicationSchemeResourcePath: String,
     ): Map<ContractId, TvmContractHandlers> {
         val communicationSchemePath = extractResource(communicationSchemeResourcePath)
         val communicationScheme = communicationSchemeFromJson(communicationSchemePath.readText())
         return communicationScheme
-    }
-
-    @Test
-    fun bounceTest() {
-        val checkerContract = extractCheckerContractFromResource(bounceCheckerPath)
-        val analyzedSender = extractFuncContractFromResource(senderBouncePath)
-        val analyzedRecipient = extractFuncContractFromResource(recipientBouncePath)
-        val communicationScheme = extractCommunicationSchemeFromResource(bounceTestScheme)
-        val options = createIntercontractOptions(communicationScheme)
-
-        val tests =
-            analyzeInterContract(
-                listOf(checkerContract, analyzedSender, analyzedRecipient),
-                startContractId = 0,
-                methodId = TvmContext.RECEIVE_INTERNAL_ID,
-                options = options,
-                concreteContractData =
-                    listOf(
-                        TvmConcreteContractData(),
-                        TvmConcreteContractData(contractC4 = Cell(BitString.of("0"))),
-                        TvmConcreteContractData(),
-                    ),
-            )
-
-        propertiesFound(
-            tests,
-            listOf { test ->
-                val checkerExitCode = test.eventsList.single { it.id == 0 }.methodResult
-                val checkerExitCodeGood = (checkerExitCode as? TvmMethodFailure)?.exitCode == 258
-                val thereWereThreeFailedMethod =
-                    test.eventsList.count { it.methodResult is TvmMethodFailure } == 3
-                checkerExitCodeGood && thereWereThreeFailedMethod
-            },
-        )
-
-        checkInvariants(
-            tests,
-            listOf { test -> (test.result as? TvmMethodFailure)?.exitCode != 257 },
-        )
-    }
-
-    @Test
-    fun `message does not bounce on soft failure`() {
-        val checkerContract = extractCheckerContractFromResource(bounceCheckerPath)
-        val analyzedSender = extractFuncContractFromResource(senderBouncePath)
-        val analyzedRecipient = extractFuncContractFromResource(recipientBounceSoftFailurePath)
-        val communicationScheme = extractCommunicationSchemeFromResource(bounceTestScheme)
-        val options = createIntercontractOptions(communicationScheme)
-
-        val tests =
-            analyzeInterContract(
-                listOf(checkerContract, analyzedSender, analyzedRecipient),
-                startContractId = 0,
-                methodId = TvmContext.RECEIVE_INTERNAL_ID,
-                options = options,
-                concreteContractData =
-                    listOf(
-                        TvmConcreteContractData(),
-                        TvmConcreteContractData(contractC4 = Cell(BitString.of("0"))),
-                        TvmConcreteContractData(),
-                    ),
-            )
-
-        assertTrue(tests.tests.isNotEmpty())
-        checkInvariants(
-            tests,
-            listOf { test ->
-                val receivedBounceOnSoftFailureExitCode = 258
-                val messageDidNotBounce =
-                    (test.result as? TvmMethodFailure)?.exitCode != receivedBounceOnSoftFailureExitCode
-                val softFailureOccurred = test.result is TvmExecutionWithSoftFailure
-                messageDidNotBounce && softFailureOccurred
-            },
-        )
-    }
-
-    @Test
-    fun bounceFormatTest() {
-        val checkerContract = extractCheckerContractFromResource(bounceFormatChecker)
-        val analyzedSender = extractFuncContractFromResource(bounceFormatContract)
-        val analyzedRecipient = extractFuncContractFromResource(recipientBouncePath)
-        val communicationScheme = extractCommunicationSchemeFromResource(bounceFormatScheme)
-        val options = createIntercontractOptions(communicationScheme)
-        val tests =
-            analyzeInterContract(
-                listOf(checkerContract, analyzedSender, analyzedRecipient),
-                startContractId = 0,
-                methodId = TvmContext.RECEIVE_INTERNAL_ID,
-                options = options,
-                concreteContractData =
-                    listOf(
-                        TvmConcreteContractData(),
-                        TvmConcreteContractData(contractC4 = Cell(BitString.of("0"))),
-                        TvmConcreteContractData(),
-                    ),
-            )
-
-        propertiesFound(
-            tests,
-            // the target contract should change its persistent data
-            listOf { test -> (test.result as? TvmMethodFailure)?.exitCode == 255 },
-        )
-        checkInvariants(
-            tests,
-            listOf { test ->
-                test.eventsList.mapNotNull { it.methodResult as? TvmMethodFailure }.all { failedResult ->
-                    val formatViolationExitCodes = 257..265
-                    failedResult.exitCode !in formatViolationExitCodes
-                }
-            },
-        )
     }
 
     private fun createIntercontractOptions(communicationScheme: Map<ContractId, TvmContractHandlers>): TvmOptions =
