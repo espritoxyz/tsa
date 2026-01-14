78d53a2f89336ee3f93d9ed9707ad44b5f789b38
metametamoon <64495509+metametamoon@users.noreply.github.com>
2026-01-14 19:09:50 +0300

    Support for Blueprint format of BoC (#206)
    * Support for blueprint format
    
    * Fixed problems
    
    * Fixed a test
    
    * Deleted unused file

--- tsa-core/src/main/kotlin/org/ton/bytecode/TsaContractCode.kt
+++ tsa-core/src/main/kotlin/org/ton/bytecode/TsaContractCode.kt
@@ -1,9 +1,12 @@
 package org.ton.bytecode
 
+import kotlinx.serialization.json.Json
 import org.ton.boc.BagOfCells
 import org.usvm.machine.toTvmCell
 import java.nio.file.Path
+import kotlin.io.path.extension
 import kotlin.io.path.readBytes
+import kotlin.io.path.readText
 
 data class TsaContractCode(
     val mainMethod: TvmMainMethod,
@@ -14,10 +17,24 @@ data class TsaContractCode(
     var isContractWithTSACheckerFunctions: Boolean = false
 
     companion object {
+        @OptIn(ExperimentalStdlibApi::class)
         fun construct(bocFilePath: Path): TsaContractCode {
-            val bocAsByteArray = bocFilePath.readBytes()
+            val bocAsByteArray =
+                when (bocFilePath.extension) {
+                    "json" -> {
+                        val json = Json.decodeFromString<Map<String, String>>(bocFilePath.readText())
+                        val hexString =
+                            json["hex"]
+                                ?: error("Bad json in boc file $bocFilePath: no 'hex' key at the top level")
+                        hexString.hexToByteArray()
+                    }
+
+                    else -> {
+                        bocFilePath.readBytes()
+                    }
+                }
             val cell = BagOfCells(bocAsByteArray).roots.first().toTvmCell()
-            val tvmContractCode = disassembleBoc(bocFilePath)
+            val tvmContractCode = disassembleBoc(bocAsByteArray)
             return construct(tvmContractCode, cell, parentCode = null)
         }
 
