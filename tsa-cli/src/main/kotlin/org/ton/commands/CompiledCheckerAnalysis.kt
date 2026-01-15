9e5d6c32f492090206bea702633fe4a56b362808
tochilinak <35286460+tochilinak@users.noreply.github.com>
2026-01-15 15:33:20 +0300

    Added CLI command for compiled custom checker (#213)

---
+++ tsa-cli/src/main/kotlin/org/ton/commands/CompiledCheckerAnalysis.kt
@@ -0,0 +1,32 @@
+package org.ton.commands
+
+import com.github.ajalt.clikt.parameters.options.help
+import com.github.ajalt.clikt.parameters.options.multiple
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.required
+import com.github.ajalt.clikt.parameters.types.path
+import org.ton.bytecode.TsaContractCode
+import org.usvm.machine.BocAnalyzer
+
+class CompiledCheckerAnalysis : AbstractCheckerAnalysis("custom-checker-compiled") {
+    override val checkerContract: TsaContractCode by lazy {
+        BocAnalyzer.loadContractFromBoc(checkerContractPath).also {
+            it.isContractWithTSACheckerFunctions = true
+        }
+    }
+    override val contractsToAnalyze: List<TsaContractCode> by lazy {
+        analyzedContracts.map {
+            BocAnalyzer.loadContractFromBoc(it)
+        }
+    }
+
+    private val checkerContractPath by option("--checker")
+        .path(mustExist = true, canBeFile = true, canBeDir = false)
+        .help("The path to the checker contract (in BoC format).")
+        .required()
+
+    private val analyzedContracts by option("-c", "--contract")
+        .path(mustExist = true, canBeFile = true, canBeDir = false)
+        .help("Contract to analyze (in BoC format).")
+        .multiple(required = true)
+}
