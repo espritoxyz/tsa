518d815dea8ca2d997356d6847e2eba8b42eb546
tochilinak <35286460+tochilinak@users.noreply.github.com>
2026-02-18 15:23:02 +0300

    CLI timeout option for opcode extraction (#277)

--- tsa-cli/src/main/kotlin/org/ton/commands/OpcodeExtraction.kt
+++ tsa-cli/src/main/kotlin/org/ton/commands/OpcodeExtraction.kt
@@ -1,13 +1,17 @@
 package org.ton.commands
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.help
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
+import com.github.ajalt.clikt.parameters.types.int
 import com.github.ajalt.clikt.parameters.types.path
 import org.usvm.machine.BocAnalyzer
+import org.usvm.machine.DEFAULT_OPCODE_EXTRACTION_TIMEOUT_SECONDS
 import org.usvm.machine.TvmOpcodeExtractor
 import kotlin.io.path.writeText
+import kotlin.time.Duration.Companion.seconds
 
 class OpcodeExtraction :
     CliktCommand(
@@ -24,10 +28,15 @@ class OpcodeExtraction :
         .required()
         .help("Path to a file with the found opcodes.")
 
+    private val timeout by option("-t", "--timeout")
+        .int()
+        .default(DEFAULT_OPCODE_EXTRACTION_TIMEOUT_SECONDS)
+        .help("Timeout for BFS symbolic analysis in seconds.")
+
     override fun run() {
         val extractor = TvmOpcodeExtractor()
         val code = BocAnalyzer.loadContractFromBoc(bocPath)
-        val opcodes = extractor.extractOpcodes(code)
+        val opcodes = extractor.extractOpcodes(code, timeout = timeout.seconds)
         val result = opcodes.joinToString(separator = "\n")
         outputPath.writeText(result)
     }
