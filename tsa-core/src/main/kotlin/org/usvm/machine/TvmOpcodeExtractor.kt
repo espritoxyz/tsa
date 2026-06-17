095b24e12df9886a7f514118b8a75f1222673a5e
tochilinak <35286460+tochilinak@users.noreply.github.com>
2026-06-17 20:18:55 +0300

    Changes in opcode extractor logic (#333)

--- tsa-core/src/main/kotlin/org/usvm/machine/TvmOpcodeExtractor.kt
+++ tsa-core/src/main/kotlin/org/usvm/machine/TvmOpcodeExtractor.kt
@@ -3,6 +3,7 @@ package org.usvm.machine
 import org.ton.bytecode.MethodId
 import org.ton.bytecode.TsaContractCode
 import org.usvm.PathSelectionStrategy
+import org.usvm.UExpr
 import org.usvm.forkblacklists.UForkBlackList
 import org.usvm.machine.state.TvmState
 import org.usvm.machine.state.TvmTerminated
@@ -11,6 +12,8 @@ import org.usvm.machine.state.input.RecvInternalInput
 import org.usvm.machine.state.killCurrentState
 import org.usvm.machine.state.sliceLoadIntTlb
 import java.math.BigInteger
+import kotlin.random.Random
+import kotlin.random.nextLong
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
 
@@ -19,6 +22,9 @@ const val DEFAULT_OPCODE_EXTRACTION_TIMEOUT_SECONDS = 20
 class TvmOpcodeExtractor(
     val opcodeLength: Int = 32,
 ) {
+    private val random = Random(0)
+    private val randomOpcode = random.nextLong(0L, 1L shl opcodeLength)
+
     fun extractOpcodes(
         code: TsaContractCode,
         methodId: MethodId = TvmContext.RECEIVE_INTERNAL_ID,
@@ -99,20 +105,39 @@ class TvmOpcodeExtractor(
                 sizeBits = opcodeLength,
                 isSigned = false,
             ) { value ->
-                val concreteValue = state.models.first().eval(value)
+                checkSat(value eq randomOpcode.toBv257())
+                    ?: run {
+                        // get here if [value == random opcode] couldn't be satisfied (maybe due to unknown)
 
-                assert(
-                    value neq concreteValue,
-                ) ?: run {
-                    // get here if [value != concreteValue] couldn't be satisfied (maybe due to unknown)
-                    opcodes.add(concreteValue.bigIntValue())
-                    return@sliceLoadIntTlb
-                }
+                        opcodes += listPossibleOpcodes(this, value)
+
+                        return@sliceLoadIntTlb
+                    }
 
                 // to skip postprocess phase
                 killCurrentState()
             }
 
             return opcodes
+        }
+
+    private fun listPossibleOpcodes(
+        scope: TvmStepScopeManager,
+        value: UExpr<TvmContext.TvmInt257Sort>,
+        limit: Int = 10,
+    ): List<BigInteger> =
+        with(scope.ctx) {
+            if (limit <= 0) {
+                return emptyList()
+            }
+            val concreteValue =
+                scope.calcOnState {
+                    models.first().eval(value)
+                }
+
+            scope.assert(value neq concreteValue)
+                ?: return listOf(concreteValue.bigIntValue())
+
+            return listPossibleOpcodes(scope, value, limit - 1) + concreteValue.bigIntValue()
         }
 }
